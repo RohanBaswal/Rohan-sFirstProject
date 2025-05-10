29ff04662974a03b28f22640aa9fad09a2dfb3c7
Rohan Baswal <vbaswal988@gmail.com>
2025-05-10 12:24:34 +0530

    initial commit

---
+++ public/js/map.js
@@ -0,0 +1,19 @@
+
+    mapboxgl.accessToken = mapToken;
+
+    const map = new mapboxgl.Map({
+        container: 'map', // container ID
+        style: "mapbox://styles/mapbox/streets-v12",
+        center: listing.geometry.coordinates, // starting position [lng, lat]
+        zoom: 9 // starting zoom
+    });
+
+
+    const marker = new mapboxgl.Marker({color: "red"})
+    .setLngLat(listing.geometry.coordinates) //listing.geometry.coordinates
+    .setPopup (
+      new mapboxgl.Popup({offset: 25}).setHTML(
+        `<h4><b>${listing.title}</b></h4><p><b>Exact Location will be provided after booking!</b></p>`))
+    .addTo(map);
+
+  
